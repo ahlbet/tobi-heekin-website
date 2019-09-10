f91eaec69a217cdecca5015541fbf628eb4e22f9
Dylan Stratton <dm.stratton94@gmail.com>
2019-09-09 20:56:54 -0500

    color update

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,8 +1,11 @@
 export default function sketch(p) {
-  let r, g, b
+  let r, g, b, rOff, gOff, bOff
 
   p.setup = function() {
-    p.createCanvas(600, 15)
+    p.createCanvas(600, 20)
+    rOff = p.random(10)
+    gOff = p.random(10)
+    bOff = p.random(10)
   }
 
   p.myCustomRedrawAccordingToNewPropsHandler = function(props) {
@@ -12,9 +15,9 @@ export default function sketch(p) {
   }
 
   p.draw = function() {
-    r = p.noise(p.frameCount / 200, 2) * 255 + 75
-    g = p.noise(p.frameCount / 300, 2) * 255 + 75
-    b = p.noise(p.frameCount / 400, 2) * 255 + 75
+    r = p.noise(rOff, p.frameCount / 200, 2) * 255 + 75
+    g = p.noise(p.frameCount / 300, gOff, 1) * 255 + 75
+    b = p.noise(p.frameCount / 400, 3, bOff) * 255 + 75
     p.background(r, g, b)
   }
 }
